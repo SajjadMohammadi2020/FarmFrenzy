3fad85383042b8a7c33ba28396ff5c0a0b6a27d6
unknown <mohammadisajjad2017@gmail.com>
2021-06-16 23:20:08 +0430

    some useful changes in Main

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,4 @@ public class Main {
         AllOfUsers allOfUsers = AllOfUsers.getAllOfUsers_Instance();
         System.out.println(allOfUsers.users.size());
     }
-
-    public static User RegisterSignUp(){
-        System.out.printf("CHOOSE:\nREGISTER\nSIGNUP\n");
-        Scanner input = new Scanner(System.in);
-        String str= input.nextLine();
-        String[][] strings = {{"register","REGISTER"},{"signup","SIGNUP"}};
-        for (int i = 0; i < strings.length; i++) {
-            str = str.replaceAll("(?i)"+strings[i][0],strings[i][1]);
-        }
-        User user ;
-        boolean userCreated = false ;
-        while (!userCreated){
-            if(str.contains("REGISTER")){
-                System.out.println("Please Enter UserName : ");
-                String userName = input.nextLine();
-                if()
-            }else if(str.contains("SIGNUP")){
-
-            }else {
-
-            }
-        }
-    }
-
-    public static User REGISTER(AllOfUsers allOfUsers){
-
-    }
 }
